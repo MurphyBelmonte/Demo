5d3d622a6e48612bb733ba6a3a046d96d630535f
Murphy_Law <belmontemurphy@gmail.com>
2025-08-11 18:03:28 +0600

    setup csharp

--- obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c3c9ef9b44c1d48b6670d181f77d290a6207d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46aefbac41005d503b32ef3975358235fab5df0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
